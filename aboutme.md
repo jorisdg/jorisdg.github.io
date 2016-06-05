0898e119b12ace547e415fe9b74f595ba2c665fc
Joris de Gruyter <joris@degruyter.us>
2016-06-04 21:25:47 -0600

    First tests

---
+++ aboutme.md
@@ -0,0 +1,9 @@
+---
+layout: default
+title: CodeCrib - About Me
+redirect_from:
+  - aboutme.aspx
+---
+
+
+### Testing About Me
---
+++ collapsible.js
@@ -0,0 +1,20 @@
+﻿function setupCollapsible() {
+    $("div.CollapsedTitle").on("click", toggleCollapsible);
+    $("div.ExpandedTitle").on("click", toggleCollapsible);
+}
+
+function toggleCollapsible() {
+    collapsible = $(this).parent().find("div.Collapsible");
+    if (collapsible.css("display") == "none") {
+        collapsible.css("display", "block");
+        $(this).parent().find("div.CollapsedTitle").css("display", "none");
+        $(this).parent().find("div.ExpandedTitle").css("display", "block");
+    }
+    else {
+        collapsible.css("display", "none");
+        $(this).parent().find("div.CollapsedTitle").css("display", "block");
+        $(this).parent().find("div.ExpandedTitle").css("display", "none");
+    }
+}
+
+$(window).load(setupCollapsible);
